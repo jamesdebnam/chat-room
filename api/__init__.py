057bba0ea05e3740642a9cf61d91a1055afcb9f0
jamesdebnam <jamesdebnam@gmail.com>
2020-06-29 09:51:42 +0100

    added User db model

--- api/__init__.py
+++ api/__init__.py
@@ -1,10 +1,18 @@
 from flask import Flask
 from flask_restful import  Api
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 from api.resources.chat import Chat
 from api.resources.chat_list import ChatList
 
 app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 api= Api(app)
+
 
 api.add_resource(Chat, '/api/<int:id>')
 api.add_resource(ChatList, '/api')
---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+  # Secret key seems to be used for form validation i think?
+  SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+  # The flask-sqlalchemy extension takes location of api's database from the 
+  # URI config variable below
+  SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    'sqlite:///' + os.path.join(basedir, 'api.db')
+  # This is disabled, normally it would signal the app every time a change was 
+  # about to be made to the database
+  SQLALCHEMY_TRACK_MODIFICATIONS = False
