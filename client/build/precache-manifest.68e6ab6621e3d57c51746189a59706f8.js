ebac592c60dc414069f8cc7bb61164ee02b510a6
jamesdebnam <jamesdebnam@gmail.com>
2020-07-03 08:34:23 +0100

    added build files

---
+++ client/build/precache-manifest.68e6ab6621e3d57c51746189a59706f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "524453850454f739605f42e0b5d46fe6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1656b89c4b22847f4197",
+    "url": "/static/css/main.1fc15554.chunk.css"
+  },
+  {
+    "revision": "a3f143bd98f1d323287c",
+    "url": "/static/js/2.567ce44b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.567ce44b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1656b89c4b22847f4197",
+    "url": "/static/js/main.cd7e7343.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "ce82bc8868a2146adad2e6851136c9e3",
+    "url": "/static/media/edit.ce82bc88.svg"
+  }
+]);
